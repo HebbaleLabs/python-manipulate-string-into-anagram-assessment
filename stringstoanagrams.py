d7d73e4a8b4f00bcfe3372cb5ae32575e956c6ab
Vineet Reynolds Pereira <Vineet.Reynolds@gmail.com>
2019-03-10 08:23:53 +0530

    Added instructions and main method.

--- stringstoanagrams.py
+++ stringstoanagrams.py
@@ -1,3 +1,17 @@
 
-def anagram(s):
+def anagram(input_string):
+  #
+  # IMPORTANT:
+  # 1. Write your solution in this function
+  # 2. Do not make changes to the function signature
+  # 3. Use the 'pip-install' command in the IDE, to install any requirements. This takes a few seconds.
+  # 4. Use the 'run' command in the IDE, to run the main method. You can also invoke the main method from the terminal.
+  # 5. Use the 'run-tests' command in the IDE, to run unit tests. The 'pip-install' command must be run before this.
   return None
+
+def main():
+  no_of_changes_for_anagram = anagram('anna')
+  print('Number of changes for anagram: {0}\n'.format(no_of_changes_for_anagram))
+
+if __name__ == '__main__':
+  main()
